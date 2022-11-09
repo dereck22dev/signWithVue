1a333a156229e09864eb43ea128904d2e50da687
Amour22 <amourdahou@gmail.com>
2022-11-09 17:03:52 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const vueJs = new Vue({
+    data: {
+        email:'',
+        pass:'',
+        phone:'',
+        // isValidPhoneNumber: phone.startsWidth("00229") || phone.startsWidth("+229"),
+    }
+}).$mount("#app");
